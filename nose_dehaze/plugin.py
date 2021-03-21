bc2c4f989a66a3526edb9317e8cb2f18011a8657
Daniel Liu <idaniel.liu@gmail.com>
2021-03-20 21:25:47 -0700

    Quick and dirty replacement of original failure output

--- nose_dehaze/plugin.py
+++ nose_dehaze/plugin.py
@@ -1,3 +1,5 @@
+from pprint import pformat
+
 from nose.plugins import Plugin
 
 
@@ -18,3 +20,30 @@ class Dehaze(Plugin):
                 self.env_opt
             ),
         )
+
+    def formatFailure(self, test, err):
+        exc_class, exc_instance, tb = err
+
+        tb2 = tb
+        formatted_output = None
+        while tb2 and formatted_output is None:
+            frame = tb2.tb_frame
+            co_name = frame.f_code.co_name
+            if co_name == "assertEqual":
+                expected = frame.f_locals["first"]
+                actual = frame.f_locals["second"]
+                padded_newline = "\n{}".format(" " * 10)
+
+                if isinstance(expected, dict) and isinstance(actual, dict):
+                    expected = pformat(expected, width=1).replace("\n", padded_newline)
+                    actual = pformat(actual, width=1).replace("\n", padded_newline)
+
+                formatted_output = (
+                    "\n\nExpected: {expected}\n  Actual: {actual}"
+                ).format(expected=expected, actual=actual)
+
+            tb2 = tb2.tb_next
+
+        output = formatted_output or exc_instance
+
+        return (exc_class, output, tb)
